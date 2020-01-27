49a6ea9e16b6aa5a9056055d37a948eee964c8e6
draganasavanovic <dragana.savanovic@wayseven.com>
2020-01-27 16:02:20 +0100

    added image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from "@angular/core";
+import { SwiperOptions } from "swiper";
+import { SwiperComponent } from "angular2-useful-swiper";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'swiper-slider';
+  title = "swiper-slider";
+  // @ViewChild("usefulSwiper", { static: false }) usefulSwiper: SwiperComponent;
+
+  slides = [
+    // "https://via.placeholder.com/300x200/FF5733/ffffff",
+    // "https://via.placeholder.com/300x200/C70039/ffffff",
+    // "https://via.placeholder.com/300x200/900C3F/ffffff"
+    { img: "../assets/miki/miki1.jpg" },
+    { img: "../assets/miki/miki2.jpg" },
+    { img: "../assets/miki/miki3.jpg" },
+    { img: "../assets/miki/miki4.jpg" },
+    { img: "../assets/miki/miki5.jpg" },
+    { img: "../assets/miki/miki6.jpg" },
+    { img: "../assets/miki/miki7.jpg" },
+    { img: "../assets/miki/miki8.jpg" },
+    { img: "../assets/miki/miki9.jpg" },
+    { img: "../assets/miki/miki10.jpg" }
+  ];
+
+  config: SwiperOptions = {
+    autoplay: { delay: 3000 }, // Autoplay option having value in milliseconds
+    initialSlide: 0, // Slide Index Starting from 0
+    slidesPerView: 1, // Slides Visible in Single View Default is 1
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    spaceBetween: 30 // Space between each Item,
+  };
+
+  // next() {
+  //   this.usefulSwiper.swiper.slideNext();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+// import { SwiperModule } from "../../node_modules/swiper";
+
+import { Angular2UsefulSwiperModule } from "node_modules/angular2-useful-swiper";
+
+// const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+//   direction: "horizontal",
+//   slidesPerView: "auto"
+// };
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, Angular2UsefulSwiperModule],
+  providers: [
+    // {
+    //   provide: SWIPER_CONFIG,
+    //   useValue: DEFAULT_SWIPER_CONFIG
+    // }
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
